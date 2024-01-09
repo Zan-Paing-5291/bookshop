101c1b15f71562700717002f62703c309caa1ffa
Zan_Paing <zanpaing@gmail.com>
2024-01-09 10:33:30 +0630

    initital status

--- src/main/java/com/example/bookshop/BookShopApplication.java
+++ src/main/java/com/example/bookshop/BookShopApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
@@ -26,6 +27,7 @@ public class BookShopApplication {
 
     @Bean
     @Transactional
+    @Profile("data")
     public ApplicationRunner runner(){
         return r->{
             Author author1 = new Author("Zan Paing","zp@gmail.com");
